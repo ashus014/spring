0c4504ac120e5b2ca667a1f02d3b506866bac76b
ashusinghin <38337277+ashusinghin@users.noreply.github.com>
2020-01-29 22:07:43 +0530

    Injecting Literal Values

--- spring-demo-one/src/com/ashu/springdemo/CricketCoach.java
+++ spring-demo-one/src/com/ashu/springdemo/CricketCoach.java
@@ -4,6 +4,30 @@ public class CricketCoach implements Coach {
 
 	private FortuneService fortuneService;
 	
+	//add new fields for email address and team
+	private String emailAddress;
+	private String team;
+	
+	public void setEmailAddress(String emailAddress) {
+		System.out.println("Cricket coach : inside setter method - setEmailAddress");
+		this.emailAddress = emailAddress;
+	}
+
+
+	public void setTeam(String team) {
+		System.out.println("Cricket coach : inside setter method - setTeam");
+		this.team = team;
+	}
+	
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+
+
+	public String getTeam() {
+		return team;
+	}
+
 	//create a no-arg constructor
 	public CricketCoach() {
 		System.out.println("Cricket coach : inside no-arg constructor");
