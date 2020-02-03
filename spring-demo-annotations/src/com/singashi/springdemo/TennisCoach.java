cd9ce2c944db3c599e27bfd7c7ccfceaee697fae
ashusinghin <38337277+ashusinghin@users.noreply.github.com>
2020-02-03 09:54:10 +0530

    Setter and Method Injection

--- spring-demo-annotations/src/com/singashi/springdemo/TennisCoach.java
+++ spring-demo-annotations/src/com/singashi/springdemo/TennisCoach.java
@@ -8,11 +8,36 @@ public class TennisCoach implements Coach {
 
 	private FortuneService fortuneService;
 	
+	//define a default constructor
+	public TennisCoach() {
+		System.out.println("> TennisCoach - inside Default Constructor");
+	}
+	
+	
 	@Autowired
-	public TennisCoach(FortuneService theFortuneService) {
+	public void doSomeCrazyStuff(FortuneService theFortuneService) {
 		
+		System.out.println("> TennisCoach - inside doSomeCrazyStuff");
 		fortuneService = theFortuneService;
 	}
+	
+	
+	
+//	//define a setter method
+//	
+//	@Autowired
+//	public void setFortuneService(FortuneService theFortuneService) {
+//		
+//		System.out.println("> TennisCoach - inside setFortuneService");
+//		fortuneService = theFortuneService;
+//	}
+	
+	
+//	@Autowired
+//	public TennisCoach(FortuneService theFortuneService) {
+//		
+//		fortuneService = theFortuneService;
+//	}
 	
 	@Override
 	public String getDailyWorkout() {
