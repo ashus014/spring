259fde9434944a70a44aaeb612e9c199f483f35b
ashusinghin <38337277+ashusinghin@users.noreply.github.com>
2020-03-27 22:36:31 +0530

    Cookies

--- DemoApp/src/com/ashu/AddServlet.java
+++ DemoApp/src/com/ashu/AddServlet.java
@@ -5,6 +5,7 @@ import java.io.PrintWriter;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,15 +14,21 @@ import javax.servlet.http.HttpSession;
 public class AddServlet extends HttpServlet{
 	
 	
-		public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
+	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException, ServletException {
 		
 		int i = Integer.parseInt(req.getParameter("num1"));
 		int j = Integer.parseInt(req.getParameter("num2"));
 		
 		int k = i + j;
 		
-		HttpSession session = req.getSession();
-		session.setAttribute("k", k);
+		
+		Cookie cookie = new Cookie("k",k+""); 
+		res.addCookie(cookie);
+		
+		//Using Session to Send Data
+		/*
+		 * HttpSession session = req.getSession(); session.setAttribute("k", k);
+		 */
 		
 		res.sendRedirect("sq");
 		
--- DemoApp/src/com/ashu/SqServlet.java
+++ DemoApp/src/com/ashu/SqServlet.java
@@ -3,6 +3,7 @@ package com.ashu;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,10 +14,21 @@ public class SqServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
 		
 
+		int k=0;
+		Cookie cookies[] = req.getCookies();
+	
+		for(Cookie c : cookies) {
+			
+			if(c.getName().equals("k")) {
+				k = Integer.parseInt(c.getValue());
+			}
+		}
 		
-		HttpSession session = req.getSession();
 		
-		int k = (int) session.getAttribute("k");
+		/*Accepts Session 
+		 * HttpSession session = req.getSession(); int k = (int)
+		 * session.getAttribute("k");
+		 */
 
 		k = k * k;
 
