e1ab108797bd15aaa58b86583bd3cab33369c635
ashusinghin <38337277+ashusinghin@users.noreply.github.com>
2020-03-30 20:25:34 +0530

    JSTL - forEach (MVC)

--- DemoApp/WebContent/display.jsp
+++ DemoApp/WebContent/display.jsp
@@ -1,22 +1,22 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
     pageEncoding="ISO-8859-1"%>
+    
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="ISO-8859-1">
 <title>Insert title here</title>
 </head>
-<body>
+<body bgcolor="cyan">
 
-	<%
+	<c:out value="${student}"></c:out>
 	
-		String name = request.getAttribute("label").toString();
-		out.println(name);
-		
-	%>
-
-
-	${label} 
+	
+	<c:forEach items="${students}" var="s">
+		${s} <br>
+	</c:forEach>
+	
 
 </body>
 </html>
--- DemoApp/src/com/ashu/DemoServlet.java
+++ DemoApp/src/com/ashu/DemoServlet.java
@@ -1,6 +1,8 @@
 package com.ashu;
 
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -14,9 +16,9 @@ public class DemoServlet extends HttpServlet {
 
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
 
-		String name = "Ashu";
+		List<Student> studs = Arrays.asList(new Student(1,"Anu"),new Student(2,"Ashu"),new Student(3,"Akash"));
 		
-		request.setAttribute("label", name);
+		request.setAttribute("students", studs);
 		RequestDispatcher rd = request.getRequestDispatcher("display.jsp");
 		rd.forward(request, response);
 	}
